d1d15e4455550133924ce38bf805c0eef857a6d3
hsluxuryproperties <hsluxuryproperties@gmail.com>
2026-06-07 14:24:24 +0300

    fix: logo in browser tab vol 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   )

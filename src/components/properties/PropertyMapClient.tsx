b32801398fad89083290c46962701cabfa78ef6b
hsluxuryproperties <hsluxuryproperties@gmail.com>
2026-06-12 16:21:27 +0300

    fix cirlce

--- src/components/properties/PropertyMapClient.tsx
+++ src/components/properties/PropertyMapClient.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic'
 
 const PropertyMap = dynamic(
   () => import('@/components/properties/PropertyMap'),
-  { ssr: false }
+  { ssr: false, loading: () => <p>Loading map...</p> }
 )
 
 export default PropertyMap
